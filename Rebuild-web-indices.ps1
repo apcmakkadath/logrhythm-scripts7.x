e20fe1b7793471cecc60f0b48f6793a5e4cae1ad
apcmakkadath <43706701+apcmakkadath@users.noreply.github.com>
2023-05-10 16:37:46 +0400

    Update Rebuild-web-indices.ps1

--- Rebuild-web-indices.ps1
+++ Rebuild-web-indices.ps1
@@ -30,15 +30,13 @@ $userName = $credential.UserName.Replace("\","")
 $pass = $credential.GetNetworkCredential().password  
 
 ### Stopping Logrhythm web seervices
-Write-Output "Stopping LogRhythm web services" | red
-Stop-Service -DisplayName "LogRhythm Web Console API"
-Stop-Service -DisplayName "LogRhythm Web Console UI"
-Stop-Service -DisplayName "LogRhythm Web Indexer"
-Stop-Service -DisplayName "LogRhythm Web Services Host API"
+Write-Output "Stopping LogRhythm web services..." | red
+Stop-Service -DisplayName "LogRhythm*Web*" 
+Get-Service -DisplayName "LogRhythm*Web*" | select DisplayName,Status
 pause
 
 ### Renaming existing indices
-Write-Output "Renaming existing indices" | red
+Write-Output "Renaming existing indices..." | red
 $renamedtime = (Get-Date).AddMinutes(-242).ToString("yyyy-MM-ddTHH-mm-00Z")
 cd "C:\tmp\indices"
 Rename-Item .\1F4779BA-307E-4F65-9947-3AFA882EE06B .\1F4779BA-307E-4F65-9947-3AFA882EE06B_$renamedtime #Alarm
@@ -49,17 +47,14 @@ Rename-Item .\0F2ABEC2-CC9F-4833-838D-7A177001A037 .\0F2ABEC2-CC9F-4833-838D-7A1
 Rename-Item .\990F503D-D5C5-46A8-BEE0-306165F67830 .\990F503D-D5C5-46A8-BEE0-306165F67830_$renamedtime #Global state index
 pause
 
-### Deleting web indices database tables 
-Write-Output "Deleting web indices database tables"
+### Deleting web indices tables from LogRhythmEMDB 
+Write-Output "Deleting web indices database tables..."
 invoke-sqlcmd -query "DELETE FROM [LogRhythmEMDB].[dbo].[WebTask] WHERE [TaskID] IN ('1F4779BA-307E-4F65-9947-3AFA882EE06B','E3FFCBF1-7981-4916-A5AE-AAA4332D2CB5','4585D33E-C058-4AFA-89CE-ED2BF4B7823D','689D0EF7-5F6A-412E-A47B-315472948C08','0F2ABEC2-CC9F-4833-838D-7A177001A037','990F503D-D5C5-46A8-BEE0-306165F67830')" -database $DB -serverinstance $servername -username $username -password $pass
 invoke-sqlcmd -query "DELETE FROM [LogRhythmEMDB].[dbo].[WebTaskToUser] WHERE [WebTaskID] IN ('1F4779BA-307E-4F65-9947-3AFA882EE06B','E3FFCBF1-7981-4916-A5AE-AAA4332D2CB5','4585D33E-C058-4AFA-89CE-ED2BF4B7823D','689D0EF7-5F6A-412E-A47B-315472948C08','0F2ABEC2-CC9F-4833-838D-7A177001A037','990F503D-D5C5-46A8-BEE0-306165F67830')" -database $DB -serverinstance $servername -username $username -password $pass
 pause
 
 ### Starting LogRhythm web services
-Write-Output "Starting LogRhythm web services" | green
-Start-Service -DisplayName "LogRhythm Web Console API"
-Start-Service -DisplayName "LogRhythm Web Console UI"
-Start-Service -DisplayName "LogRhythm Web Indexer"
-Start-Service -DisplayName "LogRhythm Web Services Host API"
+Write-Output "Starting LogRhythm web services..." | green
+Start-Service -DisplayName "LogRhythm*Web*" 
+Get-Service -DisplayName "LogRhythm*Web*" | select DisplayName,Status
 Write-Output "Web indices rebuild complete" | green
-
