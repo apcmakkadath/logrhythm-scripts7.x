0d3eeacc1887c07fb2e208631d755c78accc6e69
apcmakkadath <43706701+apcmakkadath@users.noreply.github.com>
2023-05-13 13:28:29 +0400

    Update file

--- test.ps1
+++ test.ps1
@@ -14,7 +14,8 @@ function Upload-FileToGitHub {
     $ContentEncoded = [System.Convert]::ToBase64String($Content)
     
     # Construct the API URL
-    $Url = "https://api.github.com/repos/$Owner/$Repo/contents/$(Split-Path -Leaf $FilePath)"
+    $FileName = Split-Path -Leaf $FilePath
+    $Url = "https://api.github.com/repos/$Owner/$Repo/contents/$FileName"
     
     # Construct the request headers
     $Headers = @{
@@ -22,19 +23,26 @@ function Upload-FileToGitHub {
         "Accept" = "application/vnd.github.v3+json"
     }
     
-    # Construct the request payload
-    $Payload = @{
-        "message" = "Upload file"
-        "content" = $ContentEncoded
-        "branch" = $Branch
-    } | ConvertTo-Json
-    
     try {
+        # Get the current file information
+        $FileInfo = Invoke-RestMethod -Uri $Url -Headers $Headers
+        
+        # Retrieve the SHA hash
+        $Sha = $FileInfo.sha
+        
+        # Construct the request payload
+        $Payload = @{
+            "message" = "Update file"
+            "content" = $ContentEncoded
+            "branch" = $Branch
+            "sha" = $Sha
+        } | ConvertTo-Json
+        
         # Send the PUT request
         $Response = Invoke-RestMethod -Uri $Url -Headers $Headers -Method Put -Body $Payload
         
         # Check the response status
-        if ($Response.StatusCode -eq 201) {
+        if ($Response.commit.sha) {
             Write-Host "File uploaded successfully!"
         } else {
             Write-Host "Failed to upload file. Error:" $Response
@@ -50,6 +58,7 @@ $Owner = "apcmakkadath"
 $Repo = "logrhythm-scripts7.x"
 $FilePath = "C:\Users\apc\Documents\desk\scripts\test.ps1"
 $Branch = "main"  # Replace with the branch name you want to upload to
-$Token = "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"
+$Token = "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"
+
 
 Upload-FileToGitHub -Owner $Owner -Repo $Repo -FilePath $FilePath -Branch $Branch -Token $Token
