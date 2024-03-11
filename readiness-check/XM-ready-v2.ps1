ed1938b74293590a9674c9d1cddb5041630b4d4d
apcmakkadath <43706701+apcmakkadath@users.noreply.github.com>
2024-03-11 10:18:02 +0400

    Update and rename XM-ready.ps1 to XM-ready-v2.ps1

--- readiness-check/XM-ready-v2.ps1
+++ readiness-check/XM-ready-v2.ps1
@@ -1,41 +1,9 @@
-﻿<### Script written by Aravind for Setting and reading values for system optimization
-Applicable to version above 7.4.X to 7.10.x
+<### Script written by Aravind for Setting and reading values for system optimization
+Applicable to version above 7.6
 Send your feedbacks to apcmakkadath@gmail.com
 ####>
 
-### Run script as Admin
-Function Check-RunAsAdministrator {
-  #Get current user context
-  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  
-  #Check user is running the script is member of Administrator Group
-  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
-  {
-       Write-host "Script is running with Administrator privileges!"
-  }
-  else
-    {
-       #Create a new Elevated process to Start PowerShell
-       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
- 
-       # Specify the current script path and name as a parameter
-       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
- 
-       #Set the Process to elevated
-       $ElevatedProcess.Verb = "runas"
- 
-       #Start the new elevated process
-       [System.Diagnostics.Process]::Start($ElevatedProcess)
- 
-       #Exit from the current, unelevated, process
-       Exit
- 
-    }
-}
- 
-#Check Script is running with Elevated Privileges
-Check-RunAsAdministrator
-
+function setxm{
 ### function color
 function green
 {
@@ -49,75 +17,22 @@ function red
 
 ### Setting OR reading values for system optimization
 set-executionpolicy -scope LocalMachine unrestricted -Force
-write-output "Powershell Execution policy is" | green
-get-executionpolicy | red
-pause
 
-write-output "Hostname :" | green
-hostname | red
-pause
 
-Write-Output "Windows defender status" | green
-Get-MpComputerStatus | select *enabled 
-pause
-
-write-output "Windows Dender AV Status" | green ## Modified to show the status only
-Get-MpPreference | findstr "Realtime"
-pause
-cls
-
-Write-Output 'DateTime:' | green
-date 
-pause
-
-Write-Output 'TimeZone' | green
-Get-TimeZone
-pause
-
-Write-Output "Dot net frameowrk version:" | green
-(Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Version | red
-pause
-
-ping $(Hostname) 
-pause
-
-Write-Output "Disk drives" | green
-$drives = Get-WmiObject Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3 -and $_.FileSystem -ne $null}
-
-foreach ($drive in $drives) {
-    Write-Output "Drive: $($drive.DeviceID)"
-    Write-Output "Volume Name: $($drive.VolumeName)"
-    Write-Output "File System: $($drive.FileSystem)"
-    Write-Output "Total Size: $([math]::Round($drive.Size / 1GB, 2)) GB"
-    Write-Output "Free Space: $([math]::Round($drive.FreeSpace / 1GB, 2)) GB"
-    Write-Output "--------------------------"
-}
-pause
 FSUTIL.EXE 8dot3name set c: 1
 FSUTIL.EXE 8dot3name set d: 1 
 FSUTIL.EXE 8dot3name set l: 1 
 FSUTIL.EXE 8dot3name set t: 1 
 FSUTIL.EXE 8dot3name set s: 1 
-pause
 
-write-output "IP Address(s)" | green
-ipconfig 
-pause
-
-write-output "Firewall status:" | green
-Get-NetFirewallProfile | findstr /r "^Name Enabled" | red
 New-NetFirewallRule -DisplayName "LogRhythm required connections tcp" -Direction Inbound -Protocol TCP -LocalPort 443, 1433, 8300, 8301, 8501, 8076  -Action Allow -Profile @("Domain", "Private", "Public")
 New-NetFirewallRule -DisplayName "LogRhythm required connections udp" -Direction Inbound -Protocol UDP -LocalPort 443, 1433, 8300, 8301, 8501, 8076  -Action Allow -Profile @("Domain", "Private", "Public")
-write-output "Update windows"
-pause
 
 write-output "Update windows" | green
 wuauclt /update
-pause
 
 Write-Output "Turning off DEP" | green
 cmd.exe /c "bcdedit.exe /set {current} nx AlwaysOff" | red
-pause
 
 Write-Output 'Adjusting vfx for best performance' | green
 $path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects'
@@ -130,9 +45,8 @@ try {
 catch {
     New-ItemProperty -Path $path -Name 'VisualFXSetting' -Value 2 -PropertyType 'DWORD'
     }
-pause
 
-Write-Output "Service management" | green 
+Write-Output "Setting service startup" | green 
 stop-service -force -name Themes
 set-service -Name Themes -Status Stopped -StartupType Disabled
 stop-service -force -name Spooler
@@ -145,12 +59,11 @@ stop-service -force -name BTAGService
 set-service -Name BTAGService -Status Stopped -StartupType Disabled
 stop-service -force -name bthserv
 set-service -Name bthserv -Status Stopped -StartupType Disabled
-pause
 
 write-output "Change startup" | green
 msconfig
 
-
+Write-Output "Set shortcuts" | green
 $TargetFile = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell_ise.exe"
 $ShortcutFile = "$env:Public\Desktop\poweshell_ise.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
@@ -169,17 +82,50 @@ $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
 $Shortcut.Save()
-pause
- 
 
 Write-Output "Setting UAC" | green
 Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
-pause
 
 Write-Output "Attempting TCP/IP Reset" | green
 ipconfig /flushdns
 ipconfig /registerdns
 #netsh winsock reset #Possibly Resets the NIC card and IP settings
 #netsh int ip reset #Possibly Resets the NIC card and IP settings
-
 Write-Output "Reboot the System" | red
+write-output "Fetching results" | green
+write-output "------------------------------------------------------------------------------"
+pause
+}
+
+function showresult{
+###collecting values
+$psexecpolicy = Get-ExecutionPolicy
+$datetime = date 
+$tz = Get-TimeZone
+$dotnetversion = (Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Version
+$pingresult = ping $(Hostname) -n 1 | findstr "from"
+### showing results
+write-output "Powershell Execution policy: $psexecpolicy"
+write-output "Computer Name: $env:ComputerName"
+write-output "Defender status:"
+Get-MpComputerStatus | select *enabled
+Get-MpPreference | findstr "Realtime"
+Write-Output "DateTime: $datetime" 
+Write-Output "Timezone is $tz"
+Write-Output "Dot net version: $dotnetversion"
+Write-Output "Ping result $pingresult"
+Write-Output "Disk drives" 
+$drives = Get-WmiObject Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3 -and $_.FileSystem -ne $null}
+foreach ($drive in $drives) {
+   write-output "$($drive.DeviceID) Drive $([math]::Round($drive.FreeSpace / 1GB, 2))GB Free of $([math]::Round($drive.Size / 1GB, 2))GB"
+}
+ipconfig | findstr "Address"
+ipconfig /all | findstr "Gateway"
+ipconfig /all | findstr "DNS"
+Get-NetFirewallProfile | findstr /r "^Name Enabled" 
+write-output "Review results"
+pause
+}
+
+setxm
+showresult
